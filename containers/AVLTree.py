5c8621cd85c25244d30843e24770f6871f647a92
Nate Stringham <n8stringham@gmail.com>
2021-07-22 12:42:32 -0600

    fix linting

--- containers/AVLTree.py
+++ containers/AVLTree.py
@@ -56,11 +56,10 @@ class AVLTree(BST):
         if node is None:
             return True
         else:
-            return all([
-                        AVLTree._balance_factor(node) in [0, -1, 1],
+            return all([AVLTree._balance_factor(node) in [0, -1, 1],
                         AVLTree._is_avl_satisfied(node.left),
                         AVLTree._is_avl_satisfied(node.right),
-                      ])
+                        ])
 
     @staticmethod
     def _left_rotate(node):
